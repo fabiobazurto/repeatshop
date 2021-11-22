907026c1e4df75da57951d952c52772ecca08947
Fabio Bazurto Blacio <fabio.bazurto@gmail.com>
2021-11-22 01:43:17 -0500

    Adding data to view

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import FullReload from 'vite-plugin-full-reload'
 import RubyPlugin from 'vite-plugin-ruby'
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
   plugins: [
+    FullReload(['config/routes.rb', 'app/views/**/*']),
     RubyPlugin(),
+    WindiCSS({
+      config: __dirname + "/windi.config.ts",
+      root: __dirname
+    }),
+    reactRefresh(),
   ],
 })
+
+

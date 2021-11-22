1001e7c7642de8bcff745d57e6f6c09bdb90b82b
Fabio Bazurto Blacio <fabio.bazurto@gmail.com>
2021-11-22 14:23:04 -0500

    Addint root paths
    Acked-by: Fabio Bazurto Blacio <fabio.bazurto@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
-  get 'dashboard/statistics'  
+  get 'dashboard/statistics'
+
+  root to: "dashboard#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
